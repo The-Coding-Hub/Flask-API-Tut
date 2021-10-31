2420f22fe5290530c230e30046d08df3053f98d5
Prameya <yourcodinghub.py@gmail.com>
2021-10-31 17:15:08 +0530

    Bug Fix

--- js/addTodo.js
+++ js/addTodo.js
@@ -1,4 +1,4 @@
-const url = "http://127.0.0.1:5000";
+const url = "https://api-todolist-tch.herokuapp.com";
 
 const addTodo = () => {
   let myForm = document.getElementById('addForm');
--- js/fetchTodos.js
+++ js/fetchTodos.js
@@ -1,4 +1,4 @@
-const url = "http://127.0.0.1:5000";
+const url = "https://api-todolist-tch.herokuapp.com";
 
 const fillDetails = (sno) => {
   fetch(`${url}/api/specific-todo/${sno}`).then(data => {
